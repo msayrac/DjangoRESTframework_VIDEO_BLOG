cc1f91024b9af657542cce943d1ae78a70da298d
msayracode <msayracode@gmail.com>
2026-05-08 20:48:54 +0300

    DRF video blog section 1 start

---
+++ haberler/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from haberler.models import *
+# Register your models here.
+
+
+
+class MakaleAdmin(admin.ModelAdmin):
+    #Tablo sutunlarını yazıayoruz
+    list_display = ('yazar','baslik','sehir','yayinlanma_tarihi','aktif','yaratilma_tarihi','guncellenme_tarihi')
+
+    list_filter = ('baslik','aciklama','yazar','sehir')
+
+admin.site.register(Makale,MakaleAdmin)
