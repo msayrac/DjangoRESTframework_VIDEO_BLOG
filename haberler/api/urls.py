c76558f04cd9335e98da18f7749aae86d8dd7e67
msayracode <msayracode@gmail.com>
2026-05-15 23:47:31 +0300

    APIView Class

--- haberler/api/urls.py
+++ haberler/api/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from haberler.api import views as api_views
 
 urlpatterns = [
-    path('makaleler/', api_views.makale_list_create_api_view, name='makale-listesi'),
-    path('makaleler/<pk>/', api_views.makale_list_create_api_view, name='makale-listesi'),
+    path('makaleler/', api_views.MakaleListCreateAPIView.as_view(), name='makale-listesi'),
+    path('makaleler/<int:pk>/', api_views.MakaleDetailAPIView.as_view(), name='makale-detay'),
 ]
+
+
+
+# function based views
+# urlpatterns = [
+#     path('makaleler/', api_views.makale_list_create_api_view, name='makale-listesi'),
+#     path('makaleler/<int:pk>/', api_views.makale_detail_api_view, name='makale-detay'),
+# ]
+
 
--- haberler/api/views.py
+++ haberler/api/views.py
@@ -5,50 +5,108 @@ from rest_framework.decorators import api_view
 from haberler.models import Makale
 from haberler.api.serializers import MakaleSerializer
 
-@api_view(['GET', 'POST'])
-def makale_list_create_api_view(request):
-    
-    if request.method == 'GET':
-        makaleler = Makale.objects.filter(aktif=True) # nesnelerden olusan bir query set -> sorgu kumesi var
-        serializer = MakaleSerializer(makaleler, many=True) # serializer te kbir verisetinden olusturduk
+# class views
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+
+class MakaleListCreateAPIView(APIView):
+    def get(self, request):
+        makeleler = Makale.objects.filter(aktif=True)
+        serializer = MakaleSerializer(makeleler, many=True)
         return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        serializer = MakaleSerializer(data=request.data) # json serialize edildi
+
+    def post(self, request):
+        serializer = MakaleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET','PUT','DELETE'])
-def makale_detail_api_view(request,pk):
-    try:
-        makale_instance = Makale.objects.get(pk=pk)
-    except Makale.DoesNotExist:
-        return Response(
-            {
-                'errors': {
-                    'code':404,
-                    'message': f'Böyle bir id ({pk}) ile ilgili makale bulunamadı.'
-                }
-            },
-            status=status.HTTP_404_NOT_FOUND
-        )
-
-    if request.method == 'GET':
-        serializer = MakaleSerializer(makale_instance)
+class MakaleDetailAPIView(APIView):
+    
+    def get_object(self,pk):
+        makale_instance =get_object_or_404(Makale, pk=pk)
+        return makale_instance
+    
+    def get(self,request, pk):
+        makale = self.get_object(pk=pk)
+        serializer = MakaleSerializer(makale)
         return Response(serializer.data)
     
-    elif request.method == 'PUT':
-        serializer = MakaleSerializer(makale_instance, data=request.data)
+    def put(self, request, pk):
+        makale = self.get_object(pk=pk)
+        serializer =MakaleSerializer(makale, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        makale = self.get_object(pk=pk)
+        makale.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+# function based view
+# @api_view(['GET', 'POST'])
+# def makale_list_create_api_view(request):
     
-    elif request.method == 'DELETE':
-        pass
+#     if request.method == 'GET':
+#         makaleler = Makale.objects.filter(aktif=True) # nesnelerden olusan bir query set -> sorgu kumesi var
+#         serializer = MakaleSerializer(makaleler, many=True) # serializer te kbir verisetinden olusturduk
+#         return Response(serializer.data)
+    
+#     elif request.method == 'POST':
+#         serializer = MakaleSerializer(data=request.data) # json serialize edildi
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# @api_view(['GET','PUT','DELETE'])
+# def makale_detail_api_view(request, pk):
+#     try:
+#         makale_instance = Makale.objects.get(pk=pk)
+#     except Makale.DoesNotExist:
+#         return Response(
+#             {
+#                 'errors': {
+#                     'code':404,
+#                     'message': f'Böyle bir id ({pk}) ile ilgili makale bulunamadı.'
+#                 }
+#             },
+#             status=status.HTTP_404_NOT_FOUND
+#         )
+
+#     if request.method == 'GET':
+#         serializer = MakaleSerializer(makale_instance)
+#         return Response(serializer.data)
+    
+#     elif request.method == 'PUT':
+#         serializer = MakaleSerializer(makale_instance, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+#     elif request.method == 'DELETE':
+#         makale_instance.delete()
+#         return Response(
+#             {
+#                 'islem': {
+#                     'code': 204,
+#                     'message': f'({pk}) id numaralı makale silinmiştir.'
+#                 }
+#             },
+#             status=status.HTTP_204_NO_CONTENT
+#         )
 
 
 
